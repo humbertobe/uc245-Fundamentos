4331d038383f4db4a6032e9439b4194b905d3230
humbertobe <humbertobe@gmail.com>
2026-03-23 22:58:42 +0000

    1 parte

---
+++ Fundamentos01/Program.cs
@@ -0,0 +1,13 @@
+namespace Fundamentos01;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Inicio da Aula01\n");
+        Aula01 aula01 = new Aula01();
+        aula01.ExecutarAula();
+
+    }
+
+}
